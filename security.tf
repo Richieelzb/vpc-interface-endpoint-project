79196d25169a0c1bbca57b4990a85f1b127c002c
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-06 16:12:03 +0200

    creating 2

--- security.tf
+++ security.tf
@@ -11,11 +11,11 @@ resource "aws_security_group" "private-sg-ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+   egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.10.0.0/16"]
   }
 
   tags = {
