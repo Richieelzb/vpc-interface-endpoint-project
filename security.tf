96d62cd89af5c3be151165ed14c44c3c11fc1ed2
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-06 18:37:50 +0200

    creating

--- security.tf
+++ security.tf
@@ -49,6 +49,14 @@ resource "aws_security_group" "ec2_connect_sg" {
   description = "Security group for EC2 Instance Connect Endpoint"
   vpc_id      = module.vpc1.vpc_id
 
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.10.0.0/16"]
+  }
+
   egress {
     from_port   = 22
     to_port     = 22
