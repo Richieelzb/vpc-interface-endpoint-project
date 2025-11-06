c5f239cfac8a48b3c6ea92019d28d727a42b4cce
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-06 12:36:16 +0200

    creating

--- ec2-private.tf
+++ ec2-private.tf
@@ -1,13 +1,12 @@
-resource "aws_instance" "ec2-private-vpc1" {
+resource "aws_instance" "ec2-private-1" {
   ami = data.aws_ami.my-data-ami.id
-  //ami = "ami-0bc691261a82b32bc"
   instance_type = var.instance-type-list[0]
   key_name      = var.key-pair
-  subnet_id     = module.vpc1.private_subnets[0]
+  subnet_id     = module.vpc1.private_subnets[1]
   // user_data              = file("${path.module}/app-install.sh") 
-  vpc_security_group_ids = [aws_security_group.private-sg-vpc1.id]
+  vpc_security_group_ids = [aws_security_group.private-sg-ec2.id]
   count                  = 1
   tags = {
-    Name = "ec2-private-vpc1"
+    Name = "ec2-private"
   }
 }
--- security.tf
+++ security.tf
@@ -1,8 +1,6 @@
-/////////////////VPC 1 SG/////////////////
-
-resource "aws_security_group" "private-sg-vpc1" {
-  name        = "allow-pvt-sg-1"
-  description = "allow-pvt-sg-1"
+resource "aws_security_group" "private-sg-ec2" {
+  name        = "allow-pvt-sg-ec2"
+  description = "allow-pvt-sg-ec2"
   vpc_id      = module.vpc1.vpc_id
 
   ingress {
@@ -21,33 +19,25 @@ resource "aws_security_group" "private-sg-vpc1" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "pvt-ec2-sg"
   }
 }
 
-//////////////////VPC 2 SG /////////////////
 
-resource "aws_security_group" "private-sg-vpc2" {
-  name        = "allow-pvt-sg-2"
-  description = "allow-pvt-sg-2"
-  vpc_id      = module.vpc2.vpc_id
+resource "aws_security_group" "private-sg-endpoint" {
+  name        = "allow-pvt-sg-endpoint"
+  description = "allow-pvt-sg-endpoint"
+  vpc_id      = module.vpc1.vpc_id
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.10.0.0/16"]
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "pvt-ec2-endpoint"
   }
 }
