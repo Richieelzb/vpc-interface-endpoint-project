954077e31d6984bf1e49b20a21ac43e4e61c5bc1
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-06 14:31:25 +0200

    creating

--- vpc-endpoint-interface.tf
+++ vpc-endpoint-interface.tf
@@ -3,7 +3,7 @@ resource "aws_vpc_endpoint" "ec2" {
   service_name      = "com.amazonaws.ap-south-1.sqs"
   vpc_endpoint_type = "Interface"
   
-  subnet_ids = module.vpc1.private_subnets[2]
+  subnet_ids = [module.vpc1.private_subnets[2]]
 
   security_group_ids = [
     aws_security_group.private-sg-endpoint.id
