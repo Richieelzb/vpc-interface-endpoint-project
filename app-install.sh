72652b470682a6767c2eeb7c562692a0f3ae75b0
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-08 00:24:40 +0200

    create

--- app-install.sh
+++ app-install.sh
@@ -6,6 +6,7 @@ sudo systemctl enable httpd.service
 sudo echo "This is the Saas application" > /var/www/html/index.html
 
 sudo yum install unzip -y
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
